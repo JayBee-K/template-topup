34e93c9b3362d837944b2c64303bbd03b2b960c2
Jay B <killerdrug007@gmail.com>
2021-03-10 16:52:57 +0700

    First commit

---
+++ assets/js/theme-script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+	let windowWidth = $(window).width();
+	if (windowWidth < 992) {
+		// $('header .header-navigation > ul').prepend('<li><a href="javascript:void(0)" class="d-flex d-lg-none" id="close-menu"><i class="fas fa-times"></i></a></li>');
+		$("header .header-navigation > ul > li > ul").each(function (index) {
+			$(this).prev().attr({
+				"href": "#subMenu" + index,
+				"data-toggle": "collapse"
+			});
+			$(this).attr({
+				"id": "subMenu" + index,
+				"class": "collapse list-unstyled mb-0",
+				"data-parent": "#hasMenu"
+			});
+		})
+	}
+	
+	function callMenu() {
+		if ($('body').hasClass('show-header-navigation')) {
+			$('body').removeClass('show-header-navigation');
+		} else {
+			$('body').addClass('show-header-navigation');
+		}
+	}
+	
+	$(document).on("click", "header .header-navigation > a, .header-overlay", function () {
+		callMenu();
+	});
+	
+	const myBanner = new Swiper('#swiper-banner', {
+		loop: true,
+		spaceBetween: 50,
+		speed: 250,
+		effect: 'fade',
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		}
+	});
+})
