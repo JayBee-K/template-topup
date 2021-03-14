87c5a8ecf956a07ca6e5bac49bb7f93c8e39cb72
Jay B <killerdrug007@gmail.com>
2021-03-14 20:27:20 +0700

    Check

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
+	$(window).scroll(function () {
+		var top = $(document).scrollTop();
+		var height = 0;
+		
+		if (top > height) {
+			$('.template-1-header').addClass('header-sticky');
+		} else {
+			$('.template-1-header').removeClass('header-sticky');
+		}
+	});
+	
 	let windowWidth = $(window).width();
 	if (windowWidth < 992) {
 		// $('header .header-navigation > ul').prepend('<li><a href="javascript:void(0)" class="d-flex d-lg-none" id="close-menu"><i class="fas fa-times"></i></a></li>');
-		$("header .header-navigation > ul > li > ul").each(function (index) {
+		$(".template-1-header .navigation > ul > li > ul").each(function (index) {
 			$(this).prev().attr({
 				"href": "#subMenu" + index,
 				"data-toggle": "collapse"
@@ -23,7 +34,7 @@ $(document).ready(function () {
 		}
 	}
 	
-	$(document).on("click", "header .header-navigation > a, .header-overlay", function () {
+	$(document).on("click", "#callMenu, .header-overlay", function () {
 		callMenu();
 	});
 	
@@ -65,5 +76,62 @@ $(document).ready(function () {
 				spaceBetween: 20,
 			},
 		}
+	});
+	
+	$('.createRow').click(function () {
+		html = `<div class="row">
+									<div class="col-md-3 mb-3">
+										<div class="form-group">
+											<label for="network" class="template-1-label text-dark">Nhà mạng</label>
+											<select class="form-control" id="network">
+												<option name="viettel">Viettel</option>
+												<option name="vinaphone">Vinaphone</option>
+												<option name="mobile">Mobile</option>
+												<option name="gate">Gate</option>
+												<option name="zing">Zing</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-md-3 mb-3">
+										<div class="form-group">
+											<label for="code" class="template-1-label text-dark">Mã thẻ</label>
+											<input type="text" class="form-control" id="code" placeholder="">
+										</div>
+									</div>
+									<div class="col-md-3 mb-3">
+										<div class="form-group">
+											<label for="seri" class="template-1-label text-dark">Mã Seri</label>
+											<input type="text" class="form-control" id="seri" placeholder="">
+										</div>
+									</div>
+									<div class="col-md-2 col-9 mb-3">
+										<div class="form-group">
+											<label for="price" class="template-1-label text-dark">Mệnh giá</label>
+											<select class="form-control" id="price">
+												<option name="-1">--- Mệnh giá ---</option>
+												<option name="10000">10.000 đ</option>
+												<option name="20000">20.000 đ</option>
+												<option name="30000">30.000 đ</option>
+												<option name="50000">50.000 đ</option>
+												<option name="100000">100.000 đ</option>
+												<option name="200000">200.000 đ</option>
+												<option name="300000">300.000 đ</option>
+												<option name="500000">500.000 đ</option>
+												<option name="1000000">1.000.000 đ</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-md-1 col-3 mb-3">
+										<a href="javascript:void(0)"
+										   class="deleteRow template-1-btn-circle ml-auto mt-33px rounded-circle btn btn-danger template-1-btn">
+											<i class="fas fa-times"></i>
+										</a>
+									</div>
+								</div>`;
+		$('#form-expand').append(html)
+	});
+	
+	$(document).on('click', '.deleteRow', function () {
+		$(this).closest('.row').remove();
 	});
 })
